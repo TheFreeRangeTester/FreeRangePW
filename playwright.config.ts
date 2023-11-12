e0d7a80a01731e4d2c4ea8cb9c697416c473efdd
Pato Miner <patominer@gmail.com>
2023-11-13 11:42:28 +1300

    Trace solo si falla

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://thefreerangetester.github.io/sandbox-automation-testing/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on',
+    trace: 'retain-on-failure',
     video: 'retain-on-failure',
   },
 
