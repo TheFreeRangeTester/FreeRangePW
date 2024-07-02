43fc8811c23be39092467d033b839ba292fca845
Pato Miner <patominer@gmail.com>
2024-07-02 13:55:35 +1200

    Updates

--- tests/FreeRangeTesters.spec.ts
+++ tests/FreeRangeTesters.spec.ts
@@ -1,46 +1,31 @@
-import { test, Browser, Page, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-(async () => {
-  let browser: Browser;
-  let page: Page;
+test.describe('Navegación en www.freerangetesters.com', () => {
+  const secciones = [
+    { nombre: 'Cursos', url: '/cursos', tituloEsperado: 'Cursos' },
+    { nombre: 'Udemy', url: '/udemy', tituloEsperado: 'Udemy' },
+    { nombre: 'Recursos', url: '/recursos', tituloEsperado: 'Recursos' },
+    { nombre: 'Mentorías', url: '/mentoria-1-1-con-pato', tituloEsperado: 'Mentoría personalizada de avance de carrera para testers de software' },
+    { nombre: 'Blog', url: '/blog', tituloEsperado: 'Free Range Testers' }
+    // Otros elementos de la matriz aquí...
+  ];
 
-  test.describe('Navegación en www.freerangetesters.com', () => {
-
-    const secciones = [
-      { nombre: 'Cursos', url: '/cursos', tituloEsperado: 'Cursos' },
-      { nombre: 'Udemy', url: '/udemy', tituloEsperado: 'Udemy' },
-      { nombre: 'Recursos', url: '/recursos', tituloEsperado: 'Recursos' },
-      { nombre: 'Blog', url: '/login', tituloEsperado: 'Acceder a Free Range Testers' }
-      // Agrega más secciones si es necesario
-    ];
-    for (const seccion of secciones) {
-      test(`Validar redirección a la sección "${seccion.nombre}"`, async ({ page }) => {
-        await test.step(`Estando yo en la web principal www.freerangetesters.com`, async () => {
-          page.goto('https://www.freerangetesters.com');
-          await expect(page).toHaveTitle('Free Range Testers');
-        });
-
-        await test.step(`Cuando hago click en "${seccion.nombre}"`, async () => {
-          page.locator('#page_header').getByRole('link', { name: seccion.nombre, exact: true }).click();
-          await page.waitForURL(`**${seccion.url}`);
-        });
-
-        await test.step(`Soy redirigido a la sección de título "${seccion.tituloEsperado}"`, async () => {
-          await expect(page).toHaveTitle(seccion.tituloEsperado);
-
-          page.getByText('banana').click();
-
-          page
-            .getByRole('listitem')
-            .filter({ hasText: 'banana' }).click();
-
-          page.getByRole('listitem').last();
-
-        });
+  for (const seccion of secciones) {
+    test(`Validar redirección a la sección "${seccion.nombre}"`, async ({ page }) => {
+      await test.step(`Estando yo en la web principal www.freerangetesters.com`, async () => {
+        await page.goto('https://www.freerangetesters.com');
       });
-    }
 
-  })
+      await test.step(`Cuando hago click en "${seccion.nombre}"`, async () => {
+        const [response] = await Promise.all([
+          page.waitForResponse(response => response.url().includes(seccion.url) && response.status() === 200),
+          page.locator('#page_header').getByRole('link', { name: seccion.nombre, exact: true }).click(),
+        ]);
+      });
 
-
-})(); 
+      await test.step(`Soy redirigido a la sección de título "${seccion.tituloEsperado}"`, async () => {
+        await expect(page).toHaveTitle(seccion.tituloEsperado);
+      });
+    });
+  }
+});
